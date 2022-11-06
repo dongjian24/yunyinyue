2f5531916bbb27a623320a7965665e8e5242d72b
DongJian <1398488772@qq.com>
2022-11-06 23:05:37 +0800

    update personal

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,20 +1,76 @@
 // pages/personal/personal.js
+import request from '../../utils/request'
+
+let startY = 0
+let moveY = 0
+let moveDistance = 0
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        coverTransform: 'translateY(0)',
+        coveTransition: '',
+        userInfo: {},
+        recentList: []
     },
-
+    handleTouchStart(event) {
+        startY = event.touches[0].clientY
+        this.setData({
+            coveTransition: ''
+        })
+    },
+    handleTouchMove(event) {
+        moveY = event.touches[0].clientY
+        moveDistance = moveY - startY
+        if (moveDistance <= 0) {
+            return
+        }
+        if (moveDistance > 80) {
+            moveDistance = 80
+        }
+        this.setData({
+            coverTransform: `translateY(${moveDistance}rpx)`
+        })
+    },
+    handleTouchEnd() {
+        this.setData({
+            coverTransform: 'translateY(0)',
+            coveTransition: 'transform 1s linear'
+        })
+    },
+    toLogin() {
+        wx.navigateTo({
+            url: '/pages/login/login',
+        })
+    },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-
+        let userInfo = wx.getStorageSync('userInfo')
+        if (userInfo) {
+            this.setData({
+                userInfo: JSON.parse(userInfo)
+            })
+        }
+        this.getUserRecentList(this.data.userInfo.userId)
     },
-
+    async getUserRecentList(userId) {
+        let result = await request('/user/record', {
+            uid: userId,
+            type: 0
+        })
+        let index = 0
+        let recentList = result.allData.slice(0, 10).map(item => {
+            item.id = index++
+            return item
+        })
+        this.setData({
+            recentList
+        })
+    },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
